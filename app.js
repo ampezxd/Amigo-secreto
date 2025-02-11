86419c640d63ca26b1c05ea4da404dbc599d6743
Andres Gutierrez <anfegumos2005@gmail.com>
2025-02-11 00:52:57 -0500

    Se corrigio un error en la función agregarAmigo, no estaba almacenando el valor en el array

--- app.js
+++ app.js
@@ -4,22 +4,40 @@ let nombreDeAmigos = [];
 
 //Función para agregar los nombres de los amigos al arreglo o array 
 function agregarAmigo() {
-    if (document.getElementById('amigo').value == '') {
+    let inputAmigo = document.getElementById('amigo');
+    let nombre = inputAmigo.value.trim();
+
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+    
+    nombreDeAmigos.push(nombre);
+    console.log(nombreDeAmigos);
+    limpiarCampoDeEntrada();
+    listaNombres();
+
+    /*if (document.getElementById('amigo').value == '') {
         alert("Por favor, inserte un nombre.");
         return;
     }else {
         nombreDeAmigos.push(document.getElementById('amigo'));
         console.log(nombreDeAmigos);
         limpiarCampoDeEntrada();
-    }
+    }*/
 }
 
 function listaNombres() {
-    let liNombres = document.getElementById('nombreAmigos');
+    let liNombres = document.getElementById('listaAmigos');
     liNombres.innerHTML = ""; //`${liNombres}`
 
-    for (let i = 0; i < nombreDeAmigos.length; i++){
-        console.log(nombreDeAmigos[i]);
+    for (let i = 0; i < listaAmigos.length; i++){
+        const listaLi = document.createElement("li");
+        listaLi.Li.textContent = nombreDeAmigos[i];
+        liNombres.appendChild(li);
+        console.log(document.getElementById('listaAmigos')); // ¿Devuelve null?
+
+
     }
 }
 
